60865bdeec92d4af16c044ecea8abbd3c1688c49
JM-Lemmi <jm.lemmerich@gmail.com>
2022-07-04 16:09:03 +0200

    add logging to grades.sh

--- grades-docker/grades.sh
+++ grades-docker/grades.sh
@@ -8,5 +8,8 @@ fi
 /usr/bin/diff grades.txt /tmp/grades.txt > /tmp/diff.txt
 if [ -s /tmp/diff.txt ]; then
     (echo -e "From: Grades <${USERNAME}>\nSubject: Grades changed\n\n"; cat /tmp/diff.txt; echo -e '\n\nAll grades:\n'; cat /tmp/grades.txt) | /usr/sbin/sendmail $NOTIFY_RECIPIENT
+    echo "$(date +"%b %d %H:%M:%S") $HOSTNAME grades.sh[$$]: Sent mail to ${NOTIFY_RECIPIENT}."
+else
+    echo "$(date +"%b %d %H:%M:%S") $HOSTNAME grades.sh[$$]: No changes detected."
 fi
 mv /tmp/grades.txt grades.tx
